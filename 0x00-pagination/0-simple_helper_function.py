481be2cef60bf532f0e5c78c1b6b7d06038ac9fc
noah-brunate <noahbrunate9@gmail.com>
2023-10-23 07:08:27 -0700

    pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script with a function index_range that takes two
+integer arguments page and page_size
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    function return aa tuple of the start and end index
+    of the requested page content
+    """
+
+    return ((page - 1) * page_size, page * page_size)
