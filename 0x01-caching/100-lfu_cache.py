c1ed26b40bacbf47ed20aedcd14c78aa8d502d8a
noah-brunate <noahbrunate9@gmail.com>
2023-10-28 02:38:43 -0700

    caching advanced

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Module describes the LFUCache class
+"""
+
+
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ class in herites from BseCaching
+        - is a caching system
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_items = {}
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+            if key in self.cache_items.keys():
+                self.cache_items[key] += 1
+            else:
+                self.cache_items[key] = 0
+
+            if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+                sorted_list = sorted(self.cache_items.items(),
+                                     key=lambda x: x[1])
+                k, v = sorted_list[0]
+                print(f"DISCARD: {k}")
+                del self.cache_data[k]
+                del self.cache_items[k]
+
+    def get(self, key):
+        """ Get an item by key
+        """
+
+        if key in self.cache_data.keys():
+            return self.cache_data[key]
+
+        return None
